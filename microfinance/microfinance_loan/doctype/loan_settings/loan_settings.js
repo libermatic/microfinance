05d78ee2e714a99c3b2972cb20af7e5d4ec1df6e
Sun Howwrongbum <sun@adrenosapient.com>
2017-12-21 09:06:12 +0530

    Loan Settings: set field queries

--- microfinance/microfinance_loan/doctype/loan_settings/loan_settings.js
+++ microfinance/microfinance_loan/doctype/loan_settings/loan_settings.js
@@ -2,7 +2,18 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Loan Settings', {
-	refresh: function(frm) {
-
-	}
+  refresh: function(frm) {
+    frm.fields_dict['loan_account'].get_query = doc => ({
+      filters: {
+        root_type: 'Asset',
+        is_group: false,
+      },
+    });
+    frm.fields_dict['interest_income_account'].get_query = doc => ({
+      filters: {
+        root_type: 'Income',
+        is_group: false,
+      },
+    });
+  },
 });
