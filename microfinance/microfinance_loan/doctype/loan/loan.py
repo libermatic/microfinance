05074dd11e15c4d1620a3230f0424c9d1dbcfed8
Sun Howwrongbum <sun@adrenosapient.com>
2017-12-27 22:47:47 +0530

    Loan: update hooks and status update method

--- microfinance/microfinance_loan/doctype/loan/loan.py
+++ microfinance/microfinance_loan/doctype/loan/loan.py
@@ -5,15 +5,24 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.utils import flt
+# from frappe.utils.dateutils import parse_date
+from frappe.utils.data import getdate, today, add_months, date_diff, get_last_day
 from frappe.model.document import Document
 import math
 
 class Loan(Document):
-	def add_principal(self, amount):
-		amount = flt(amount)
+	def before_submit(self):
+		self.disbursement_status = 'Sanctioned'
+		self.recovery_status = 'Not Started'
+	def update_from_application(self, application):
+		amount = flt(application.amount)
 		if amount < 0:
 			frappe.throw('Cannot decrease principal')
 		self.loan_principal += amount
+		if not self.calculation_slab == application.calculation_slab:
+			self.calculation_slab == application.calculation_slab
+		if self.disbursement_status == 'Fully Disbursed':
+			self.disbursement_status == 'Partially Disbursed'
 		self.save()
 
 @frappe.whitelist()
@@ -39,13 +48,41 @@ def get_outstanding_principal(loan):
 			SELECT sum(debit) - sum(credit)
 			FROM `tabGL Entry`
 			WHERE {}
-		""".format(" and ".join(cond)))[0][0]
+		""".format(" AND ".join(cond)))[0][0]
 	return principal
 
+def get_interval(day_of_month, date_obj):
+	try:
+		start_date = date_obj.replace(day=day_of_month)
+	except ValueError:
+		start_date = add_months(date_obj, -1).replace(day=day_of_month)
+	if date_diff(date_obj, start_date) < 0:
+		start_date = add_months(start_date, -1)
+	try:
+		end_date = date_obj.replace(day=day_of_month)
+	except ValueError:
+		end_date = get_last_day(date_obj)
+	if date_diff(end_date, date_obj) < 0:
+		end_date = add_months(end_date, 1)
+	return start_date, end_date
+
 @frappe.whitelist()
-def get_interest_amount(loan):
+def get_interest_amount(loan=None, posting_date=today()):
+	if not loan:
+		return None
+	posting_date = getdate(posting_date)
+	billing_date = frappe.get_value('Loan', loan, 'billing_date')
+	start_date, end_date = get_interval(day_of_month=billing_date.day, date_obj=posting_date)
+	paid_amount = frappe.db.sql("""
+			SELECT sum(gl.credit - gl.debit)
+			FROM `tabGL Entry` AS gl, `tabLoan` AS lt
+			WHERE gl.against_voucher = lt.name
+			AND gl.account = lt.interest_income_account
+			AND gl.posting_date BETWEEN '{start_date}' AND '{end_date}'
+			AND lt.name = '{loan}'
+		""".format(loan=loan, start_date=start_date, end_date=end_date))[0][0] or 0
 	principal = get_outstanding_principal(loan)
 	rate, slab = frappe.get_value('Loan', loan, ['rate_of_interest', 'calculation_slab'])
 	if slab:
 		principal = math.ceil(principal / slab) * slab
-	return principal * rate / 100.0
+	return principal * rate / 100.0 - paid_amount
