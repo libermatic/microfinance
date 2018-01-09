d411b22a8a34a873c33bd73a0db54f71549b69b2
Sun Howwrongbum <sun@adrenosapient.com>
2018-01-09 12:16:50 +0530

    Loan: add method get_billing_period
    - test get_interval
    - type conversions for other methods

--- microfinance/microfinance_loan/doctype/loan/loan.py
+++ microfinance/microfinance_loan/doctype/loan/loan.py
@@ -5,10 +5,11 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.utils import flt
-from frappe.utils.data import getdate, today, add_months, date_diff, get_last_day
+from frappe.utils.data import getdate, today, add_months, add_days, date_diff, get_last_day
 from frappe.model.document import Document
 from frappe.contacts.doctype.address.address import get_default_address
 import math
+from datetime import date
 
 class Loan(Document):
 	def before_submit(self):
@@ -38,15 +39,17 @@ def get_undisbursed_principal(loan):
 	return flt(full_principal) - flt(disbursed_principal)
 
 @frappe.whitelist()
-def get_outstanding_principal(loan, date=today()):
+def get_outstanding_principal(loan, posting_date=today()):
 	'''Get outstanding principal'''
+	if not isinstance(posting_date, date):
+		posting_date = getdate(posting_date)
 	loan_account = frappe.get_value('Loan', loan, 'loan_account')
 	cond = [
 			"account = '{}'".format(loan_account),
 			"voucher_type = 'Journal Entry'",
 			"against_voucher_type = 'Loan'",
 			"against_voucher = '{}'".format(loan),
-			"posting_date <= '{}'".format(date),
+			"posting_date <= '{}'".format(posting_date),
 		]
 	principal = frappe.db.sql("""
 			SELECT sum(debit) - sum(credit)
@@ -56,7 +59,7 @@ def get_outstanding_principal(loan, date=today()):
 	return principal
 
 @frappe.whitelist()
-def get_recovered_principal(loan, date=today()):
+def get_recovered_principal(loan, posting_date=today()):
 	'''Get recovered principal'''
 	recovered = frappe.db.sql("""
 			SELECT SUM(principal)
@@ -67,6 +70,8 @@ def get_recovered_principal(loan, date=today()):
 
 def get_interval(day_of_month, date_obj):
 	'''Returns start and end date of the interval'''
+	if not isinstance(date_obj, date):
+		date_obj = getdate(date_obj)
 	try:
 		start_date = date_obj.replace(day=day_of_month)
 	except ValueError:
@@ -79,25 +84,35 @@ def get_interval(day_of_month, date_obj):
 		end_date = get_last_day(date_obj)
 	if date_diff(end_date, date_obj) < 0:
 		end_date = add_months(end_date, 1)
+	if end_date.day >= day_of_month:
+		end_date = add_days(end_date, -1)
 	return start_date, end_date
 
 @frappe.whitelist()
-def get_interest_amount(loan=None, posting_date=today()):
+def get_billing_period(loan=None, interval_date=today()):
+	'''Returns start and end date of a period'''
+	if not loan:
+		return None
+	if not isinstance(interval_date, date):
+		interval_date = getdate(interval_date)
+	billing_date = frappe.get_value('Loan', loan, 'billing_date')
+	billing_day = billing_date.day if billing_date else 1
+	start_date, end_date = get_interval(day_of_month=billing_day, date_obj=interval_date)
+	return start_date, end_date
+
+@frappe.whitelist()
+def get_interest_amount(loan=None, start_date=today(), end_date=today()):
 	'''Get interest amount'''
 	if not loan:
 		return None
-	posting_date = getdate(posting_date)
-	billing_date = frappe.get_value('Loan', loan, 'billing_date') or posting_date.replace(day=1)
-	start_date, end_date = get_interval(day_of_month=billing_date.day, date_obj=posting_date)
 	paid_amount = frappe.db.sql("""
-			SELECT sum(gl.credit - gl.debit)
-			FROM `tabGL Entry` AS gl, `tabLoan` AS lt
-			WHERE gl.against_voucher = lt.name
-			AND gl.account = lt.interest_income_account
-			AND gl.posting_date BETWEEN '{start_date}' AND '{end_date}'
-			AND lt.name = '{loan}'
+			SELECT sum(interest)
+			FROM `tabRecovery`
+			WHERE docstatus = 1
+			AND loan = '{loan}'
+			AND billing_period = '{start_date} - {end_date}'
 		""".format(loan=loan, start_date=start_date, end_date=end_date))[0][0] or 0
-	principal = get_outstanding_principal(loan, posting_date)
+	principal = get_outstanding_principal(loan, end_date)
 	rate, slab = frappe.get_value('Loan', loan, ['rate_of_interest', 'calculation_slab'])
 	if slab:
 		principal = math.ceil(principal / slab) * slab
---
+++ microfinance/microfinance_loan/doctype/loan/test_loan_utils.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017, Libermatic and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import unittest
+from datetime import date
+
+from microfinance.microfinance_loan.doctype.loan.loan import get_interval
+
+class TestLoanUtils(unittest.TestCase):
+    def test_get_interval(self):
+        '''Test correct returns'''
+        self.assertEqual(
+                get_interval(5, '2017-12-12'),
+                (date(year=2017, month=12, day=5), date(year=2018, month=1, day=4))
+            )
+        self.assertEqual(
+                get_interval(12, '2017-12-05'),
+                (date(year=2017, month=11, day=12), date(year=2017, month=12, day=11))
+            )
+        self.assertEqual(
+                get_interval(5, date(year=2017, month=12, day=12)),
+                (date(year=2017, month=12, day=5), date(year=2018, month=1, day=4))
+            )
+    def test_get_interval_known_edges(self):
+        '''Test for known edge cases'''
+        self.assertEqual(
+                get_interval(5, '2017-12-31'),
+                (date(year=2017, month=12, day=5), date(year=2018, month=1, day=4))
+            )
+        self.assertEqual(
+                get_interval(31, '2017-11-12'),
+                (date(year=2017, month=10, day=31), date(year=2017, month=11, day=30))
+            )
+        self.assertEqual(
+                get_interval(31, '2018-02-12'),
+                (date(year=2018, month=1, day=31), date(year=2018, month=2, day=28))
+            )
