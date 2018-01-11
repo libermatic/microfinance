787532b3e7cd617750a544d0173063626454c885
Sun Howwrongbum <sun@adrenosapient.com>
2018-01-11 20:02:31 +0530

    Loan Settings: auto set after wizard
    - bump to v0.0.3

--- microfinance/setup.py
+++ microfinance/setup.py
@@ -6,16 +6,41 @@ from __future__ import unicode_literals
 import frappe
 from frappe.exceptions import DuplicateEntryError
 
-def _create_account(doc, company_name):
-	abbr = frappe.get_value('Company', company_name, 'abbr')
-	return frappe.get_doc({
+def _create_account(doc, company_name, company_abbr):
+	account = frappe.get_doc({
 			'doctype': 'Account',
 			'account_name': doc['account_name'],
-			'parent_account': "{} - {}".format(doc['parent_account'], abbr),
+			'parent_account': "{} - {}".format(doc['parent_account'], company_abbr),
 			'is_group': 0,
 			'company': company_name,
 			'account_type': doc.get('account_type'),
 		}).insert(ignore_if_duplicate=True)
+	return account.name
+
+loan_settings_accounts = {
+	'loan_account': {
+			'account_name': 'Microfinance Loans',
+			'parent_account': 'Loans and Advances (Assets)',
+		},
+	'interest_income_account': {
+			'account_name': 'Interests on Loans',
+			'parent_account': 'Indirect Income',
+		}
+}
+
+def after_wizard_complete(args=None):
+	if frappe.defaults.get_global_default('country') != "India":
+		return
+	loan_settings = frappe.get_doc('Loan Settings', None)
+	loan_settings.update({ 'mode_of_payment' : 'Cash' })
+	for key, value in loan_settings_accounts.items():
+		account_name = _create_account(
+				value,
+				args.get('company_name'),
+				args.get('company_abbr')
+			)
+		loan_settings.update({ key: account_name })
+	loan_settings.save()
 
 def _set_fixtures():
 	options = frappe.get_meta('Journal Entry Account').get_field('reference_type').options
@@ -42,22 +67,6 @@ def _set_fixtures():
 			'insert_after': 'against_account',
 			'fieldtype': 'Text',
 		}).insert(ignore_if_duplicate=True)
-
-def after_wizard_complete(args=None):
-	if frappe.defaults.get_global_default('country') != "India":
-		return
-
-	company_name = frappe.defaults.get_global_default('company')
-
-	_create_account({
-			'account_name': "Microfinance Loans",
-			'parent_account': "Loans and Advances (Assets)",
-		}, company_name)
-
-	_create_account({
-			'account_name': "Interests on Loans",
-			'parent_account': "Indirect Income",
-		}, company_name)
 
 def after_install(args=None):
 	_set_fixtures()
