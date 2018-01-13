bdf69ac8633939a83ffc1a0d25abacd66dfae64d
Sun Howwrongbum <sun@adrenosapient.com>
2018-01-14 00:05:14 +0530

    Account Statement: update sql queries to use GL Entry

--- microfinance/microfinance_loan/report/account_statement/account_statement.py
+++ microfinance/microfinance_loan/report/account_statement/account_statement.py
@@ -10,9 +10,10 @@ from frappe.utils import flt
 def make_row(result):
 	row = [
 		result.get('posting_date'),
-		result.get('transaction_details'),
+		result.get('account'),
 		result.get('credit'),
 		result.get('debit'),
+		result.get('amount'),
 		result.get('remarks')
 		]
 	return row
@@ -20,60 +21,63 @@ def make_row(result):
 def execute(filters=None):
 	columns = [
 			_("Posting Date") + ":Date:90",
-			_("Details") + "::160",
+			_("Account") + ":Link/Account:160",
 			_("Credit") + ":Currency/currency:90",
 			_("Debit") + ":Currency/currency:90",
-			_("Remarks") + "::400",
+			_("Amount") + ":Currency/currency:90",
+			_("Remarks") + "::300",
 		]
 
 	loan_name = filters.get('loan')
 	from_date = filters.get('from_date')
 	to_date = filters.get('to_date')
-	interest_income_account = frappe.db.get_value(
+	excluded_accounts = frappe.db.get_value(
 			'Loan',
 			loan_name,
-			'interest_income_account'
+			['loan_account', 'interest_receivable_account']
 		)
-	results = frappe.db.sql("""
-			SELECT
-				GL.posting_date AS posting_date,
-				GL.account AS account,
-				GL.against AS against,
-				GL.credit AS credit,
-				GL.debit AS debit,
-				GL.voucher_no as voucher_no,
-				Journal.transaction_details AS transaction_details,
-				GL.remarks as remarks
-			FROM
-				`tabGL Entry` AS GL,
-				`tabJournal Entry Account` AS Journal,
-				`tabLoan` as Loan
-			WHERE GL.voucher_no = Journal.parent
-			AND GL.against_voucher='{loan}'
-			AND Journal.account = GL.account
-			AND Journal.against_account = GL.against
-			AND GL.against_voucher_type='Loan'
-			AND Loan.loan_account != GL.account
-			AND Loan.name = GL.against_voucher
-			AND GL.posting_date BETWEEN '{from_date}' AND '{to_date}'
-		""".format(loan=loan_name, from_date=from_date, to_date=to_date), as_dict=True)
+
+	conds = [
+			"against_voucher_type = 'Loan'",
+			"against_voucher = '{}'".format(loan_name),
+			"account NOT IN ({})".format(', '.join(map(lambda x: "'{}'".format(x), excluded_accounts)))
+		]
+
 	opening = frappe.db.sql("""
 			SELECT
-				sum(GL.credit) AS credit,
-				sum(GL.debit) AS debit
-			FROM `tabGL Entry` AS GL,
-					`tabLoan` as Loan
-			WHERE GL.posting_date < '{from_date}'
-			AND GL.against_voucher_type='Loan'
-			AND GL.against_voucher='{loan}'
-			AND Loan.loan_account != GL.account
-			AND Loan.name = GL.against_voucher
-		""".format(loan=loan_name, from_date=from_date), as_dict=True)
+				sum(credit) AS credit,
+				sum(debit) AS debit,
+				sum(credit - debit) as amount
+			FROM `tabGL Entry` AS GL
+			WHERE {conds} AND posting_date < '{from_date}'
+		""".format(conds=" AND ".join(conds), from_date=from_date), as_dict=True)
+
 	opening_credit = flt(opening[0].get('credit'))
 	opening_debit = flt(opening[0].get('debit'))
+	opening_amount = flt(opening[0].get('amount'))
+
 	data = [
-		[None, _("Opening"), opening_credit, opening_debit, None]
+		[None, _("Opening"), opening_credit, opening_debit, opening_amount, None]
 	]
+
+	results = frappe.db.sql("""
+			SELECT
+				account,
+				posting_date,
+				sum(credit) as credit,
+				sum(debit) as debit,
+				sum(credit - debit) AS amount,
+				remarks
+			FROM `tabGL Entry` AS remarks
+			WHERE {conds}
+			AND posting_date BETWEEN '{from_date}' AND '{to_date}'
+			GROUP BY voucher_type, voucher_no, account, remarks
+			ORDER BY name
+		""".format(
+				conds=" AND ".join(conds),
+				from_date=from_date,
+				to_date=to_date
+			), as_dict=True)
 	for result in results:
 		row = make_row(result)
 		if row:
@@ -81,7 +85,16 @@ def execute(filters=None):
 
 	total_credit = reduce((lambda a, x: a + x.get('credit')), results, 0)
 	total_debit = reduce((lambda a, x: a + x.get('debit')), results, 0)
-	data.append([None, _("Total"), total_credit, total_debit, None])
-	data.append([None, _("Closing"), opening_credit + total_credit, opening_debit + total_debit, None])
+	total_amount = reduce((lambda a, x: a + x.get('amount')), results, 0)
+
+	data.append([None, _("Total"), total_credit, total_debit, total_amount, None])
+	data.append([
+			None,
+			_("Closing"),
+			opening_credit + total_credit,
+			opening_debit + total_debit,
+			opening_amount + total_amount,
+			None
+		])
 
 	return columns, data
