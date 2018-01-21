ca70f7a752651b028ba1a7185f47b42bc53511e7
Sun Howwrongbum <sun@adrenosapient.com>
2018-01-22 01:07:11 +0530

    Add patch to move billing_period in GL Entry

---
+++ microfinance/patches/v0_1/move_billing_period_from_against_to_period.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2013, Libermatic and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+@frappe.whitelist()
+def execute():
+    frappe.reload_doctype('GL Entry')
+    loans = frappe.get_all('Loan')
+    frappe.db.sql("""
+            UPDATE
+                `tabGL Entry` as GLEntry,
+                `tabLoan` as Loan
+            SET
+                GLEntry.period = GLEntry.against,
+                GLEntry.against = NULL
+            WHERE GLEntry.against IS NOT NULL
+            AND GLEntry.period IS NULL
+            AND Loan.name IN ({})
+            AND (GLEntry.against_voucher = Loan.name OR GLEntry.voucher_no = Loan.name)
+            AND GLEntry.account IN (Loan.interest_receivable_account, Loan.loan_account)
+        """.format(", ".join(map(lambda x: "'{}'".format(x.get('name')), loans))), debug=True)
