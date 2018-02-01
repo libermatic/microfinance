c59491f9c7b84e53aa1bcc269d66b46a4ae64cd8
Sun Howwrongbum <sun@adrenosapient.com>
2018-02-02 03:30:52 +0530

    Remove utils from js build

--- microfinance/hooks.py
+++ microfinance/hooks.py
@@ -17,7 +17,7 @@ app_license = "MIT"
 # include js, css files in header of desk.html
 app_include_css = "assets/css/microfinance.css"
 # app_include_css = "assets/microfinance/css/devel.css"
-app_include_js = "assets/js/microfinance.js"
+app_include_js = ["assets/js/microfinance.js", "assets/microfinance/js/utils.js"]
 
 # include js, css files in header of web template
 # web_include_css = "/assets/microfinance/css/microfinance.css"
