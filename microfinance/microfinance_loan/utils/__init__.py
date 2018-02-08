54dc5ae93663a4ec2cef161b5e77030f8c4332ff
Sun Howwrongbum <sun@adrenosapient.com>
2018-02-08 15:32:07 +0530

    Add utils to month_diff

--- microfinance/microfinance_loan/utils/__init__.py
+++ microfinance/microfinance_loan/utils/__init__.py
@@ -3,6 +3,7 @@
 # For license information, please see license.txt
 
 from frappe.utils import getdate, add_months, get_datetime_str
+from dateutil.relativedelta import relativedelta
 
 def get_billing_date(current_date, billing_day=1):
     '''Return the next billing date from current_date'''
@@ -11,3 +12,8 @@ def get_billing_date(current_date, billing_day=1):
     if billing_date <= date_obj:
         billing_date = add_months(billing_date, 1)
     return get_datetime_str(billing_date).split(' ')[0]
+
+def month_diff(d1, d2):
+    '''Return d1 - d2 in months without the days portion'''
+    r = relativedelta(getdate(d1), getdate(d2))
+    return r.years * 12 + r.months
--- microfinance/microfinance_loan/utils/test_utils.py
+++ microfinance/microfinance_loan/utils/test_utils.py
@@ -6,7 +6,7 @@ from __future__ import unicode_literals
 import unittest
 import frappe
 
-from microfinance.microfinance_loan.utils import get_billing_date
+from microfinance.microfinance_loan.utils import get_billing_date, month_diff
 
 class TestUtils(unittest.TestCase):
 
@@ -15,21 +15,34 @@ class TestUtils(unittest.TestCase):
         actual = get_billing_date('2017-12-12')
         expected = '2018-01-01'
         self.assertEqual(actual, expected)
-
     def test_get_billing_date_before(self):
         '''Test get_billing_date when billing_day is before current_date day'''
         actual = get_billing_date('2017-12-12', 27)
         expected = '2017-12-27'
         self.assertEqual(actual, expected)
-
     def test_get_billing_date_same(self):
         '''Test get_billing_date when billing_day is equal to current_date day'''
         actual = get_billing_date('2017-12-12', 12)
         expected = '2018-01-12'
         self.assertEqual(actual, expected)
-
     def test_get_billing_date_after(self):
         '''Test get_billing_date when billing_day is after current_date day'''
         actual = get_billing_date('2017-12-12', 5)
         expected = '2018-01-05'
+        self.assertEqual(actual, expected)
+
+    def test_month_diff(self):
+        '''Test month_diff default case'''
+        actual = month_diff('2020-08-19', '2017-12-12')
+        expected = 32
+        self.assertEqual(actual, expected)
+    def test_month_diff_same_day(self):
+        '''Test month_diff when the day of the month is the same'''
+        actual = month_diff('2020-08-12', '2017-12-12')
+        expected = 32
+        self.assertEqual(actual, expected)
+    def test_month_diff_feb_leap(self):
+        '''Test month_diff when the other day is Feb 29'''
+        actual = month_diff('2020-02-29', '2018-02-28')
+        expected = 24
         self.assertEqual(actual, expected)
