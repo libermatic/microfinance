3be19b7ede85e05e8f0aa9718f9d1ff063d3a9fe
Sun Howwrongbum <sun@adrenosapient.com>
2018-02-20 21:51:29 +0530

    Add few loan_utils
    - update Recovery code formatting

--- microfinance/microfinance_loan/doctype/loan/loan_utils.py
+++ microfinance/microfinance_loan/doctype/loan/loan_utils.py
@@ -1,4 +1,5 @@
 from datetime import date
+import frappe
 from frappe.utils import cint
 from frappe.utils.data \
     import getdate, add_months, add_days, date_diff, get_last_day
@@ -39,3 +40,92 @@ def get_periods(day_of_month, date_obj, no_of_periods=5):
                 'as_text': as_text,
             })
     return intervals
+
+
+def paid_interest(loan, period):
+    interest_receivable_account = frappe.get_value(
+            'Loan',
+            loan,
+            'interest_receivable_account'
+        )
+    counterparts = map(lambda x: "'{}'".format(x.get('name')), frappe.get_all(
+        'Account',
+        {'account_type': ['in', 'Cash, Bank'], 'is_group': 0},
+        'name'
+    ))
+
+    conds = [
+        "account = '{}'".format(interest_receivable_account),
+        "against in ({})".format(", ".join(counterparts)),
+        "period = '{}'".format(period),
+        "against_voucher_type = 'Loan'",
+        "against_voucher = '{}'".format(loan),
+    ]
+
+    return frappe.db.sql("""
+        SELECT sum(credit - debit) FROM `tabGL Entry` WHERE {}
+    """.format(" AND ".join(conds)))[0][0] or 0
+
+
+def billed_interest(loan, period):
+    interest_income_account, interest_receivable_account = frappe.get_value(
+            'Loan',
+            loan,
+            ['interest_income_account', 'interest_receivable_account']
+        )
+
+    conds = [
+            "account = '{}'".format(interest_receivable_account),
+            "against = '{}'".format(interest_income_account),
+            "period = '{}'".format(period),
+            "against_voucher_type = 'Loan'",
+            "against_voucher = '{}'".format(loan),
+        ]
+
+    return frappe.db.sql("""
+        SELECT sum(debit - credit) FROM `tabGL Entry` WHERE {}
+    """.format(" AND ".join(conds)))[0][0] or 0
+
+
+def get_disbursement_between(loan, period):
+    start_date, end_date = period.split(' - ')
+    loan_account = frappe.get_value(
+            'Loan',
+            loan,
+            'loan_account'
+        )
+    conds = [
+        "account = '{}'".format(loan_account),
+        "voucher_type = 'Disbursement'",
+        "posting_date BETWEEN '{0}' AND '{1}'".format(start_date, end_date),
+        "against_voucher_type = 'Loan'",
+        "against_voucher = '{}'".format(loan),
+    ]
+    return frappe.db.sql("""
+        SELECT sum(debit - credit) FROM `tabGL Entry` WHERE {}
+    """.format(" AND ".join(conds)))[0][0] or 0
+
+
+def get_recovery_between(loan, period):
+    start_date, end_date = period.split(' - ')
+    loan_account = frappe.get_value(
+            'Loan',
+            loan,
+            'loan_account'
+        )
+    counterparts = map(lambda x: "'{}'".format(x.get('name')), frappe.get_all(
+        'Account',
+        {'account_type': ['in', 'Cash, Bank'], 'is_group': 0},
+        'name'
+    ))
+    conds = [
+        "account = '{}'".format(loan_account),
+        "against in ({})".format(", ".join(counterparts)),
+        "voucher_type = 'Recovery'",
+        "posting_date BETWEEN '{0}' AND '{1}'".format(start_date, end_date),
+        "against_voucher_type = 'Loan'",
+        "against_voucher = '{}'".format(loan),
+    ]
+    return frappe.db.sql("""
+        SELECT sum(credit - debit) FROM `tabGL Entry` WHERE {}
+    """.format(" AND ".join(conds)))[0][0] or 0
--- microfinance/microfinance_loan/doctype/recovery/recovery.py
+++ microfinance/microfinance_loan/doctype/recovery/recovery.py
@@ -5,180 +5,206 @@
 from __future__ import unicode_literals
 import frappe
 from frappe import _
-from frappe.utils import flt
+from frappe.utils import flt, today
 from erpnext.accounts.general_ledger import make_gl_entries
 from erpnext.controllers.accounts_controller import AccountsController
 from frappe.utils.data import fmt_money
 
-from microfinance.microfinance_loan.doctype.loan.loan import get_outstanding_principal, get_interest
+from microfinance.microfinance_loan.doctype.loan.loan \
+    import get_outstanding_principal, get_interest
+
 
 class Recovery(AccountsController):
-	def validate(self):
-		outstanding_principal = get_outstanding_principal(self.loan, self.posting_date)
-		if self.amount > outstanding_principal + flt(self.interest):
-			frappe.throw(_(
-					"Cannot recover more that the outstanding principal: {}"
-						.format(outstanding_principal)
-				))
+    def validate(self):
+        outstanding_principal = get_outstanding_principal(
+            self.loan, self.posting_date
+        )
+        if self.amount > outstanding_principal + flt(self.interest):
+            frappe.throw(_(
+                "Cannot recover more that the outstanding principal: \
+                    {}".format(outstanding_principal)
+            ))
 
-	def on_submit(self):
-		self.make_gl_entries()
-		update_loan_status(self.loan)
+    def on_submit(self):
+        self.make_gl_entries()
+        update_loan_status(self.loan)
 
-	def on_cancel(self):
-		self.make_gl_entries(cancel=True)
-		update_loan_status(self.loan)
+    def on_cancel(self):
+        self.make_gl_entries(cancel=True)
+        update_loan_status(self.loan)
 
-	def make_gl_entries(self, cancel=0, adv_adj=0):
-		gl_entries = []
+    def make_gl_entries(self, cancel=0, adv_adj=0):
+        gl_entries = []
 
-		unbilled = self.get_unbilled()
-		if unbilled:
-			self.add_billing_gl_entries(gl_entries, unbilled)
-		if self.interest:
-			self.add_party_gl_entries(gl_entries)
-		if self.principal:
-			self.add_loan_gl_entries(gl_entries)
-		if len(self.loan_charges) > 0:
-			self.add_charges_gl_entries(gl_entries)
+        unbilled = self.get_unbilled()
+        if unbilled:
+            self.add_billing_gl_entries(gl_entries, unbilled)
+        if self.interest:
+            self.add_party_gl_entries(gl_entries)
+        if self.principal:
+            self.add_loan_gl_entries(gl_entries)
+        if len(self.loan_charges) > 0:
+            self.add_charges_gl_entries(gl_entries)
 
-		make_gl_entries(gl_entries, cancel=cancel, adv_adj=adv_adj, merge_entries=False)
+        make_gl_entries(
+            gl_entries,
+            cancel=cancel,
+            adv_adj=adv_adj,
+            merge_entries=False
+        )
 
-	def get_gl_dict(self, args):
-		gl_dict = frappe._dict({
-				'against_voucher_type': 'Loan',
-				'against_voucher': self.loan
-			})
-		gl_dict.update(args)
-		return super(Recovery, self).get_gl_dict(gl_dict)
+    def get_gl_dict(self, args):
+        gl_dict = frappe._dict({
+                'against_voucher_type': 'Loan',
+                'against_voucher': self.loan
+            })
+        gl_dict.update(args)
+        return super(Recovery, self).get_gl_dict(gl_dict)
 
-	def get_unbilled(self):
-		unbilled = frappe.db.sql("""
-				SELECT sum(debit - credit)
-				FROM `tabGL Entry`
-				WHERE account = '{0}'
-				AND against_voucher_type = 'Loan'
-				AND against_voucher = '{1}'
-				AND period = '{2}'
-			""".format(self.interest_receivable_account, self.loan, self.billing_period))[0][0] or 0
-		if self.billing_period:
-			start_date, end_date = self.billing_period.split(' - ')
-			# interest = self.interest if self.edit_interest else get_interest(self.loan, start_date, end_date)
-			interest = get_interest(self.loan, start_date, end_date)
-			return interest - flt(unbilled)
-		return 0
+    def get_unbilled(self):
+        unbilled = frappe.db.sql("""
+                SELECT sum(debit - credit)
+                FROM `tabGL Entry`
+                WHERE account = '{0}'
+                AND against_voucher_type = 'Loan'
+                AND against_voucher = '{1}'
+                AND period = '{2}'
+            """.format(
+                self.interest_receivable_account,
+                self.loan,
+                self.billing_period)
+            )[0][0] or 0
+        if self.billing_period:
+            start_date, end_date = self.billing_period.split(' - ')
+            # interest = self.interest \
+            #     if self.edit_interest \
+            #     else get_interest(self.loan, start_date, end_date)
+            interest = get_interest(self.loan, start_date, end_date)
+            return interest - flt(unbilled)
+        return 0
 
-	def add_billing_gl_entries(self, gl_entries, unbilled):
-		gl_entries.append(
-				self.get_gl_dict({
-						'account': self.interest_income_account,
-						'credit': unbilled,
-						'cost_center': frappe.db.get_value('Loan Settings', None, 'cost_center'),
-						'against': self.customer,
-						'remarks': 'Interest for period: {}'.format(self.billing_period)
-					})
-			)
-		gl_entries.append(
-				self.get_gl_dict({
-						'account': self.interest_receivable_account,
-						'debit': unbilled,
-						'party_type': 'Customer',
-						'party': self.customer,
-						'against': self.interest_income_account,
-						'period': self.billing_period,
-					})
-			)
+    def add_billing_gl_entries(self, gl_entries, unbilled):
+        gl_entries.append(
+                self.get_gl_dict({
+                        'account': self.interest_income_account,
+                        'credit': unbilled,
+                        'cost_center': frappe.db.get_value(
+                            'Loan Settings',
+                            None,
+                            'cost_center'
+                        ),
+                        'against': self.customer,
+                        'remarks': 'Interest for period: {}'.format(
+                            self.billing_period
+                        )
+                    })
+            )
+        gl_entries.append(
+                self.get_gl_dict({
+                        'account': self.interest_receivable_account,
+                        'debit': unbilled,
+                        'party_type': 'Customer',
+                        'party': self.customer,
+                        'against': self.interest_income_account,
+                        'period': self.billing_period,
+                    })
+            )
 
-	def add_party_gl_entries(self, gl_entries):
-		gl_entries.append(
-				self.get_gl_dict({
-						'account': self.payment_account,
-						'debit': self.interest,
-						'against': self.customer,
-						'remarks': make_remarks(self.principal)
-					})
-			)
-		gl_entries.append(
-				self.get_gl_dict({
-						'account': self.interest_receivable_account,
-						'credit': self.interest,
-						'party_type': 'Customer',
-						'party': self.customer,
-						'against': self.payment_account,
-						'period': self.billing_period,
-					})
-			)
-	def add_loan_gl_entries(self, gl_entries):
-		gl_entries.append(
-				self.get_gl_dict({
-						'account': self.loan_account,
-						'credit': self.principal,
-						'against': self.payment_account,
-					})
-			)
-		gl_entries.append(
-				self.get_gl_dict({
-						'account': self.payment_account,
-						'debit': self.principal,
-						'against': self.customer,
-						'remarks': make_remarks(self.principal)
-					})
-			)
-	def add_charges_gl_entries(self, gl_entries):
-		total = 0
-		cost_center = frappe.db.get_value('Loan Settings', None, 'cost_center')
-		for row in self.loan_charges:
-			total += row.charge_amount
-			gl_entries.append(
-					self.get_gl_dict({
-							'account': row.charge_account,
-							'credit': row.charge_amount,
-							'cost_center': cost_center,
-							'remarks': row.charge
-						})
-				)
-		gl_entries.append(
-				self.get_gl_dict({
-						'account': self.payment_account,
-						'debit': total,
-						'against': self.customer,
-					})
-			)
+    def add_party_gl_entries(self, gl_entries):
+        gl_entries.append(
+                self.get_gl_dict({
+                        'account': self.payment_account,
+                        'debit': self.interest,
+                        'against': self.customer,
+                        'remarks': make_remarks(self.principal)
+                    })
+            )
+        gl_entries.append(
+                self.get_gl_dict({
+                        'account': self.interest_receivable_account,
+                        'credit': self.interest,
+                        'party_type': 'Customer',
+                        'party': self.customer,
+                        'against': self.payment_account,
+                        'period': self.billing_period,
+                    })
+            )
+
+    def add_loan_gl_entries(self, gl_entries):
+        gl_entries.append(
+                self.get_gl_dict({
+                        'account': self.loan_account,
+                        'credit': self.principal,
+                        'against': self.payment_account,
+                    })
+            )
+        gl_entries.append(
+                self.get_gl_dict({
+                        'account': self.payment_account,
+                        'debit': self.principal,
+                        'against': self.customer,
+                        'remarks': make_remarks(self.principal)
+                    })
+            )
+
+    def add_charges_gl_entries(self, gl_entries):
+        total = 0
+        cost_center = frappe.db.get_value('Loan Settings', None, 'cost_center')
+        for row in self.loan_charges:
+            total += row.charge_amount
+            gl_entries.append(
+                    self.get_gl_dict({
+                            'account': row.charge_account,
+                            'credit': row.charge_amount,
+                            'cost_center': cost_center,
+                            'remarks': row.charge
+                        })
+                )
+        gl_entries.append(
+                self.get_gl_dict({
+                        'account': self.payment_account,
+                        'debit': total,
+                        'against': self.customer,
+                    })
+            )
+
 
 def make_remarks(principal=None):
-	remarks = 'Payment received'
-	if principal:
-		remarks += '. Capital: {}'.format(fmt_money(
-				principal,
-				precision=0,
-				currency=frappe.defaults.get_user_default('currency')
-			))
-	return remarks
+    remarks = 'Payment received'
+    if principal:
+        remarks += '. Capital: {}'.format(fmt_money(
+                principal,
+                precision=0,
+                currency=frappe.defaults.get_user_default('currency')
+            ))
+    return remarks
 
-def update_loan_status(loan_name):
-	'''Method update recovery_status of Loan'''
-	disbursement_status, loan_principal, recovery_status = frappe.get_value(
-			'Loan',
-			loan_name,
-			['disbursement_status', 'loan_principal', 'recovery_status']
-		)
-	outstanding_principal = get_outstanding_principal(loan_name)
-	loan = frappe.get_doc('Loan', loan_name)
-	do_save = False
-	if disbursement_status == 'Fully Disbursed' and outstanding_principal == 0:
-		loan.clear_date = self.posting_date
-		loan.recovery_status = 'Repaid'
-		do_save = True
-	else:
-		if outstanding_principal == loan_principal and recovery_status != 'Not Started':
-			loan.recovery_status = 'Not Started'
-			do_save = True
-		elif recovery_status != 'In Progress':
-			loan.recovery_status = 'In Progress'
-			do_save = True
-		if loan.clear_date:
-			loan.clear_date = None
-			do_save = True
-	if do_save:
-		return loan.save()
-	return None
+
+def update_loan_status(loan_name, posting_date=today()):
+    '''Method update recovery_status of Loan'''
+    disbursement_status, loan_principal, recovery_status = frappe.get_value(
+            'Loan',
+            loan_name,
+            ['disbursement_status', 'loan_principal', 'recovery_status']
+        )
+    outstanding_principal = get_outstanding_principal(loan_name)
+    loan = frappe.get_doc('Loan', loan_name)
+    do_save = False
+    if disbursement_status == 'Fully Disbursed' and outstanding_principal == 0:
+        loan.clear_date = posting_date
+        loan.recovery_status = 'Repaid'
+        do_save = True
+    else:
+        if outstanding_principal == loan_principal \
+                and recovery_status != 'Not Started':
+            loan.recovery_status = 'Not Started'
+            do_save = True
+        elif recovery_status != 'In Progress':
+            loan.recovery_status = 'In Progress'
+            do_save = True
+        if loan.clear_date:
+            loan.clear_date = None
+            do_save = True
+    if do_save:
+        return loan.save()
+    return None
