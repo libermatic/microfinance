23ea7376073e191ad5e71209a4fa2d4e57867113
Sun Howwrongbum <sun@adrenosapient.com>
2018-03-16 02:05:30 +0530

    Loan: add LOan Write Off to dash board
    - remove Journal Entry

--- microfinance/microfinance_loan/doctype/loan/loan_dashboard.py
+++ microfinance/microfinance_loan/doctype/loan/loan_dashboard.py
@@ -2,44 +2,45 @@ import frappe
 from frappe import _
 
 from microfinance.microfinance_loan.doctype.loan.loan \
-	import get_undisbursed_principal, get_outstanding_principal, \
-		get_recovered_principal, get_wrote_off_principal
+    import get_undisbursed_principal, get_outstanding_principal, \
+    get_recovered_principal, get_wrote_off_principal
 
 
 def get_data(d=None):
-	return {
-		'fieldname': 'loan',
-		'non_standard_fieldnames': {
-				'Journal Entry': 'reference_name',
-			},
-		'transactions': [
-			{
-				'label': _('Payment'),
-				'items': ['Disbursement', 'Recovery']
-			},
-			{
-				'label': _('Account'),
-				'items': ['Journal Entry']
-			}
-		]
-	}
+    return {
+        'fieldname': 'loan',
+        'non_standard_fieldnames': {
+                'Journal Entry': 'reference_name',
+            },
+        'transactions': [
+            {
+                'label': _('Payment'),
+                'items': ['Disbursement', 'Recovery']
+            },
+            {
+                'label': _('Adjustment'),
+                'items': ['Loan Write Off']
+            }
+        ]
+    }
+
 
 @frappe.whitelist()
 def get_loan_chart_data(docname):
-	recovered = get_recovered_principal(docname)
-	outstanding = get_outstanding_principal(docname)
-	undisbursed = get_undisbursed_principal(docname)
-	wrote_off = get_wrote_off_principal(docname)
+    recovered = get_recovered_principal(docname)
+    outstanding = get_outstanding_principal(docname)
+    undisbursed = get_undisbursed_principal(docname)
+    wrote_off = get_wrote_off_principal(docname)
 
-	data = {
-		'labels': [
-				'Recovered', 'Outstanding', 'Undisbursed', 'Wrote Off'
-			],
-		'datasets': [
-			{
-				'title': "Total",
-				'values': [recovered, outstanding, undisbursed, wrote_off]
-			},
-		]
-	}
-	return data
+    data = {
+        'labels': [
+                'Recovered', 'Outstanding', 'Undisbursed', 'Wrote Off'
+            ],
+        'datasets': [
+            {
+                'title': "Total",
+                'values': [recovered, outstanding, undisbursed, wrote_off]
+            },
+        ]
+    }
+    return data
