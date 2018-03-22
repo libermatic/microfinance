28b6925c48b36c4be051381a86b517017a74a092
Sun Howwrongbum <sun@libermatic.com>
2018-03-22 22:06:36 +0530

    Account Statement: fix loan start date not getting rendered
    - remove customer filter
    - add prettier config

--- microfinance/microfinance_loan/report/account_statement/account_statement.js
+++ microfinance/microfinance_loan/report/account_statement/account_statement.js
@@ -21,42 +21,25 @@ frappe.query_reports['Account Statement'] = {
       width: '60px',
     },
     {
-      fieldname: 'customer',
-      label: __('Customer'),
-      fieldtype: 'Link',
-      options: 'Customer',
-    },
-    {
       fieldname: 'loan',
       label: __('Loan'),
       fieldtype: 'Link',
       options: 'Loan',
       get_query: function() {
-        const customer = frappe.query_report_filters_by_name[
-          'customer'
-        ].get_value();
-        if (customer) {
-          return { doctype: 'Loan', filters: { customer } };
-        }
-        return { doctype: 'Loan' };
+        return { doctype: 'Loan', filters: { docstatus: 1 } };
       },
       on_change: async function() {
-        const customer = frappe.query_report_filters_by_name[
-          'customer'
-        ].get_value();
-        if (!customer) {
-          const loan = frappe.query_report_filters_by_name['loan'].get_value();
-          const { message = {} } = await frappe.db.get_value('Loan', loan, [
-            'customer',
-            'customer_name',
-          ]);
-          frappe.query_report_filters_by_name['customer'].set_value(
-            message['customer']
-          );
-          frappe.query_report_filters_by_name['customer_name'].set_value(
-            message['customer_name']
-          );
-        }
+        const loan = frappe.query_report_filters_by_name['loan'].get_value();
+        const { message = {} } = await frappe.db.get_value('Loan', loan, [
+          'customer_name',
+          'posting_date',
+        ]);
+        frappe.query_report_filters_by_name['customer_name'].set_value(
+          message['customer_name']
+        );
+        frappe.query_report_filters_by_name['loan_start_date'].set_value(
+          message['posting_date']
+        );
       },
       reqd: 1,
     },
@@ -64,6 +47,12 @@ frappe.query_reports['Account Statement'] = {
       fieldname: 'customer_name',
       label: __('Customer Name'),
       fieldtype: 'Data',
+      hidden: 1,
+    },
+    {
+      fieldname: 'loan_start_date',
+      label: __('Loan Start Date'),
+      fieldtype: 'Date',
       hidden: 1,
     },
   ],
